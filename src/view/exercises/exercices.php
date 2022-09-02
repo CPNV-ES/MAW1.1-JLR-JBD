b8b14f72cd2be849c934b75e3441e7fae0a0cb48
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2022-09-02 09:54:42 +0200

    Initial Push

---
+++ src/view/exercises/exercices.php
@@ -0,0 +1,99 @@
+
+<!-- saved from url=(0052)https://stormy-plateau-54488.herokuapp.com/exercises -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><header class="heading results">
+  <section class="container">
+    <a href="https://stormy-plateau-54488.herokuapp.com/"><img src="./Exercices_files/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png"></a>
+    
+  </section>
+</header>
+
+<main class="container">
+  
+
+  
+    <title>ExerciseLooper</title>
+    <meta name="csrf-param" content="authenticity_token">
+<meta name="csrf-token" content="SfMiI550UQV9hSjd7ruUd1AErm+eHL4YphDiENsbGcRLSfL4k/t3dFbTQPpeH6NGLrG5dTeCXM1hmILS8XfKXw==">
+    
+
+    <link rel="stylesheet" media="all" href="./Exercices_files/application-264507a893987846393b8514969b89293817c54265354e63e6ab61fb46193f89.css">
+    <script src="./Exercices_files/application-212289bcba525f2374cdbd70755ea38f2cfdd35d479e9638fae0b2832fac5dac.js.téléchargement"></script>
+  
+
+  
+    <div class="row">
+  <section class="column">
+    <h1>Building</h1>
+    <table class="records">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+          <tr>
+            <td>rgt</td>
+            <td>
+                <a title="Be ready for answers" rel="nofollow" data-method="put" href="https://stormy-plateau-54488.herokuapp.com/exercises/631?exercise%5Bstatus%5D=answering"><i class="fa fa-comment"></i></a>
+              <a title="Manage fields" href="https://stormy-plateau-54488.herokuapp.com/exercises/631/fields"><i class="fa fa-edit"></i></a>
+              <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="https://stormy-plateau-54488.herokuapp.com/exercises/631"><i class="fa fa-trash"></i></a>
+            </td>
+          </tr>
+           
+      </tbody>
+    </table>
+  </section>
+
+  <section class="column">
+    <h1>Answering</h1>
+    <table class="records">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+          <tr>
+            <td>&lt;b&gt;new exercise&lt;/b&gt;</td>
+            <td>
+              <a title="Show results" href="https://stormy-plateau-54488.herokuapp.com/exercises/378/results"><i class="fa fa-chart-bar"></i></a>
+              <a title="Close" rel="nofollow" data-method="put" href="https://stormy-plateau-54488.herokuapp.com/exercises/378?exercise%5Bstatus%5D=closed"><i class="fa fa-minus-circle"></i></a>
+            </td>
+          </tr>
+         
+      </tbody>
+    </table>
+  </section>
+
+  <section class="column">
+    <h1>Closed</h1>
+    <table class="records">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+          <tr>
+            <td>Mathieu</td>
+            <td>
+              <a title="Show results" href="https://stormy-plateau-54488.herokuapp.com/exercises/365/results"><i class="fa fa-chart-bar"></i></a>
+              <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="https://stormy-plateau-54488.herokuapp.com/exercises/365"><i class="fa fa-trash"></i></a>
+            </td>
+          </tr>
+          
+      </tbody>
+    </table>
+  </section>
+</div>
+
+  
+
+
+</main></body></html>
