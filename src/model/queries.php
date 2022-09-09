b1a26c735fd014c3291dcc8d0aed78793e816c61
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2022-09-09 08:27:56 +0200

    created function to delete database entries, update database

--- src/model/queries.php
+++ src/model/queries.php
@@ -3,17 +3,22 @@ require __DIR__ . "/connect.php";
 
 function insert($title)
 {
-  $query = "INSERT INTO exercices(Title)  VALUES ('$title')";
+  $query = "INSERT INTO exercises(Title)  VALUES ('$title')";
   connect($query);
 }
 
 function selectAllExercises()
 {
-  $query = 'SELECT title FROM exercices';
+  $query = 'SELECT idExercises, title FROM exercises';
 
   $result = connect($query);
  
   return $result;
 }
 
+function delete($id){
+  $query = "DELETE FROM exercises WHERE idExercises = '$id'";
+
+  $result = connect($query);
+}
 ?>
