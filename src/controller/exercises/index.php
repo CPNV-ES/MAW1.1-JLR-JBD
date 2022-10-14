f6fb80d3e22d6a45f96e65a838a1cd68f3c20b04
habbst1 <habbst1@gmail.com>
2022-10-14 10:22:38 +0200

    update index.php for exercice

--- src/controller/exercises/index.php
+++ src/controller/exercises/index.php
@@ -3,10 +3,6 @@ require_once SOURCE_DIR.'/model/exercise_handler.php';
 
 $handler = ExerciseHandler::getInstance();
 
-//var_dump($handler->exists(129,133));
-
-
-die();
 $bag['view'] = 'view/exercises/index';
 
 $bag['exercises'] = $handler->getExercise();
